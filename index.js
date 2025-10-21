63c7906b3a92cdf61762f8121a38dd7cd2d26ec2
scain-ui <scain@midland.k12.ia.us>
2025-10-21 17:15:04 +0000

    1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+var count = 100;
 
+Document.getElementById("before-prefix-increment").innerHTML = count;
+Document.getElementById("prefix-increment").innerHTML = ++count;
+
+Document.getElementById("before-postfix-increment").innerHTML = count;
+Document.getElementById("postfix-increment").innerHTML = count++;
+
+Document.getElementById("brefore-prefix-decrement").innerHTML = count;
+Document.getElementById("prefix-decrement").innerHTML = --count;
+
+Document.getElementById("before-postfix-decrement").innerHTML = count;
+Document.getElementById("postfix-decrement").innerHTML = count--;
+Document.getElementById("result").innerHTML = counter;
